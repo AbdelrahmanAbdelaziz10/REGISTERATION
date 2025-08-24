9529db87ef76b3289823adf6401f8e1e644ab040
AbdelrahmanAbdelaziz10 <abdelazizabdelrahman373@gmail.com>
2025-08-24 10:57:08 +0300

    try to build

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://192.168.0.180:9090',
-        changeOrigin: true,
-        secure: false, // Only if you're using self-signed certificates
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+  // base: '/اسم_التطبيق/', // ضع اسم المسار الذي ستنشر تحته داخل WebSphere
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
-})
+});
